be10f605ca9954fc49a1913cd730c934e6f3abe0
remi.dormoy <remi.dormoy@accenture.com>
2023-04-07 10:20:13 +0200

    temp

---
+++ lib/activity_repository.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'activity_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$activityRepositoryHash() =>
+    r'29678eff81e80eac38a6b0f24b0aada33957f9d3';
+
+/// See also [activityRepository].
+@ProviderFor(activityRepository)
+final activityRepositoryProvider =
+    AutoDisposeProvider<ActivityRepository>.internal(
+  activityRepository,
+  name: r'activityRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$activityRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ActivityRepositoryRef = AutoDisposeProviderRef<ActivityRepository>;
+String _$httpClientHash() => r'd721c54b3ba3b33f6d735f3ba5c13582dfc13c1f';
+
+/// See also [httpClient].
+@ProviderFor(httpClient)
+final httpClientProvider = AutoDisposeProvider<Client>.internal(
+  httpClient,
+  name: r'httpClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$httpClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HttpClientRef = AutoDisposeProviderRef<Client>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
